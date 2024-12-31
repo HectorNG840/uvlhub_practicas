b36f5dcde37850514a20fadf3322e98d5cb8906b
Héctor Noguera González <hecnoggon@alum.us.es>
2024-12-31 12:24:50 +0100

    Fix: Notepad

--- app/modules/notepad/tests/test_unit.py
+++ app/modules/notepad/tests/test_unit.py
@@ -1,5 +1,10 @@
 import pytest
 
+from app import db
+from app.modules.conftest import login, logout
+from app.modules.auth.models import User
+from app.modules.profile.models import UserProfile
+
 
 @pytest.fixture(scope='module')
 def test_client(test_client):
